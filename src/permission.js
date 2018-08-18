60a030d6bee83d647825c9865c2edb2319f0acf1
wlf36 <1533759639@qq.com>
2018-08-18 13:53:30 +0800

    权限admin

--- src/permission.js
+++ src/permission.js
@@ -1,43 +1,51 @@
 import router from './router'
 import store from './store'
 import NProgress from 'nprogress' // Progress 进度条
-import 'nprogress/nprogress.css'// Progress 进度条样式
-import { Message } from 'element-ui'
-import { getToken } from '@/utils/auth' // 验权
+import 'nprogress/nprogress.css' // Progress 进度条样式
+import {
+    Message
+} from 'element-ui'
+import {
+    getToken
+} from '@/utils/auth' // 验权
 
 const whiteList = ['/login'] // 不重定向白名单
 router.beforeEach((to, from, next) => {
-  NProgress.start()
-  if (getToken()) {
-    if (to.path === '/login') {
-      next({ path: '/' })
-      NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
+    NProgress.start()
+    if (getToken()) {
+        if (to.path === '/login') {
+            next({
+                path: '/'
+            })
+            NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
+        } else {
+            // console.log(store.getters.roles)
+            if (store.getters.roles !== 'admin') { // 拉取用户权限信息
+                store.dispatch('GetInfo').then(res => {
+                    console.log('成功拉取用户权限信息')
+                    next()
+                }).catch((err) => { //拉取用户权限失败，跳出登录
+                    store.dispatch('FedLogOut').then(() => {
+                        Message.error(err || 'Verification failed, please login again')
+                        next({
+                            path: '/'
+                        })
+                    })
+                })
+            } else {
+                next()
+            }
+        }
     } else {
-      // console.log(store.getters.roles)
-      if (store.getters.roles !== 'admin') {   // 拉取用户权限信息
-        store.dispatch('GetInfo').then(res => { 
-          // console.log('成功拉取用户权限信息')
-          next()
-        }).catch((err) => { //拉取用户权限失败，跳出登录
-          store.dispatch('FedLogOut').then(() => {
-            Message.error(err || 'Verification failed, please login again')
-            next({ path: '/' })
-          })
-        })
-      } else {
-        next()
-      }
+        if (whiteList.indexOf(to.path) !== -1) {
+            next()
+        } else {
+            next('/login')
+            NProgress.done()
+        }
     }
-  } else {
-    if (whiteList.indexOf(to.path) !== -1) {
-      next()
-    } else {
-      next('/login')
-      NProgress.done()
-    }
-  }
 })
 
 router.afterEach(() => {
-  NProgress.done() // 结束Progress
+    NProgress.done() // 结束Progress
 })
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,97 +1,113 @@
-import { login, logout, getInfo } from "@/api/login";
-import { getToken, setToken, removeToken } from "@/utils/auth";
+import {
+    login,
+    logout,
+    getInfo
+} from "@/api/login";
+import {
+    getToken,
+    setToken,
+    removeToken
+} from "@/utils/auth";
 
 const user = {
-  state: {
-    token: getToken(),
-    username: "",
-    avatar: "",
-    roles: ""
-  },
+    state: {
+        token: getToken(),
+        username: "",
+        avatar: "",
+        roles: ""
+    },
 
-  mutations: {
-    SET_TOKEN: (state, token) => {
-      state.token = token;
+    mutations: {
+        SET_TOKEN: (state, token) => {
+            state.token = token;
+        },
+        SET_NAME: (state, username) => {
+            state.username = username;
+        },
+        SET_AVATAR: (state, avatar) => {
+            state.avatar = avatar;
+        },
+        SET_ROLES: (state, roles) => {
+            state.roles = roles;
+        }
     },
-    SET_NAME: (state, username) => {
-      state.username = username;
-    },
-    SET_AVATAR: (state, avatar) => {
-      state.avatar = avatar;
-    },
-    SET_ROLES: (state, roles) => {
-      state.roles = roles;
+
+    actions: {
+        // 登录
+        Login({
+            commit
+        }, userInfo) {
+            const _username = userInfo.username.trim();
+            return new Promise((resolve, reject) => {
+                login(_username, userInfo.password)
+                    .then(response => {
+                        const data = response.data;
+                        console.log(data.token)
+                        setToken(data.token); //设置cookie
+                        commit("SET_TOKEN", data.token);
+                        resolve();
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
+            });
+        },
+
+        // 获取用户信息
+        GetInfo({
+            commit,
+            state
+        }) {
+            return new Promise((resolve, reject) => {
+                getInfo(state.token)
+                    .then(response => {
+                        const data = response.data;
+                        //判断用户权限为是否为admin
+                        if (data.roles == 'admin') {
+                            commit("SET_ROLES", data.roles);
+                        } else {
+                            reject("getInfo: no permission !");
+                        }
+                        commit("SET_NAME", data.username);
+                        // commit("SET_AVATAR", data.avatar);
+                        resolve(response);
+                    })
+                    .catch(error => {                        
+                        reject(error);
+                    });
+            });
+        },
+
+        // 登出
+        LogOut({
+            commit,
+            state
+        }) {
+            return new Promise((resolve, reject) => {
+                logout(state.token)
+                    .then(() => {
+                        commit("SET_TOKEN", "");
+                        commit("SET_ROLES", []);
+                        removeToken();
+                        resolve();
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
+            });
+        },
+
+        // 前端 登出
+        FedLogOut({
+            commit
+        }) {
+            return new Promise(resolve => {
+                commit("SET_TOKEN", "");
+                removeToken();
+                resolve();
+            });
+        }
     }
-  },
-
-  actions: {
-    // 登录
-    Login({ commit }, userInfo) {
-      const _username = userInfo.username.trim();
-      return new Promise((resolve, reject) => {
-        login(_username, userInfo.password)
-          .then(response => {
-            const data = response.data;
-            // console.log(data.token)
-            setToken(data.token); //设置cookie
-            commit("SET_TOKEN", data.token);
-            resolve();
-          })
-          .catch(error => {
-            reject(error);
-          });
-      });
-    },
-
-    // 获取用户信息
-    GetInfo({ commit, state }) {
-      return new Promise((resolve, reject) => {
-        getInfo(state.token)
-          .then(response => {
-            const data = response.data;
-            
-            //判断用户权限为是否为admin
-            if (data.roles == 'admin') { 
-              commit("SET_ROLES", data.roles);
-            } else {
-              reject("getInfo: roles must be admin !");
-            }
-            commit("SET_NAME", data.username);
-            // commit("SET_AVATAR", data.avatar);
-            resolve(response);
-
-          })
-          .catch(error => {
-            reject(error);
-          });
-      });
-    },
-
-    // 登出
-    LogOut({ commit, state }) {
-      return new Promise((resolve, reject) => {
-        logout(state.token)
-          .then(() => {
-            commit("SET_TOKEN", "");
-            commit("SET_ROLES", []);
-            removeToken();
-            resolve();
-          })
-          .catch(error => {
-            reject(error);
-          });
-      });
-    },
-
-    // 前端 登出
-    FedLogOut({ commit }) {
-      return new Promise(resolve => {
-        commit("SET_TOKEN", "");
-        removeToken();
-        resolve();
-      });
-    }
-  }
 };
 
 export default user;
